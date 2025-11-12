1e9800f2c99f6ae08a87041cfabad8143bf6e7fa
Delight44-max <eboagorojahdiebube@gmail.com>
2025-11-13 00:06:14 +0100

    added

---
+++ Assignment.sql
@@ -0,0 +1,29 @@
+-- CREATE DATABASE shop_db;
+USE shop_db;
+CREATE TABLE users (
+ user_id INT AUTO_INCREMENT PRIMARY KEY,
+ username VARCHAR(100) NOT NULL UNIQUE,
+  email VARCHAR(150) NOT NULL UNIQUE,
+  password VARCHAR(225) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+ );
+
+  CREATE TABLE products (
+  product_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(150) NOT NULL,
+  description TEXT,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+ );
+
+
+CREATE TABLE order_items (
+  order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT NOT NULL,
+  product_id INT NOT NULL,
+  quantity INT NOT NULL DEFAULT 1,
+  price DECIMAL(10, 2) NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+  FOREIGN KEY (product_id) REFERENCES products(product_id) ON DELETE CASCADE
+);
