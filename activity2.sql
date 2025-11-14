3b579fff1ab6400a4d98bb25a20f777361d561dc
Delight44-max <eboagorojahdiebube@gmail.com>
2025-11-14 20:41:52 +0100

    added

--- activity2.sql
+++ activity2.sql
@@ -64,8 +64,201 @@ select SKU, SKU_Description, warehouseID, QuantityOnHand
 from INVENTORY WHERE QuantityOnHand BETWEEN 1 and 10;
 
 -- 2.31
-select SKU_Description, SKU
-from INVENTORY WHERE QuantityOnHand BETWEEN 1 and 10;
+select DISTINCT SKU, SKU_Description
+ from SKU_DATA WHERE 
+ SKU_Description LIKE '%Half-Dome%';
+ 
+ -- 2.32
+ select  DISTINCT SKU, SKU_DESCRIPTION
+ from INVENTORY
+ WHERE SKU_DESCRIPTION like '%CLIMB%';
+ 
+ -- 2.33
+ SELECT DISTINCT SKU, SKU_Description
+FROM INVENTORY
+WHERE SKU_Description LIKE '__d%';
+
+
+-- 2.34
+SELECT 
+    AVG(QuantityOnHand) AS Average_On_Hand,
+    SUM(QuantityOnHand) AS Total_On_Hand,
+    MIN(QuantityOnHand) AS Minimum_On_Hand,
+    MAX(QuantityOnHand) AS Maximum_On_Hand,
+    COUNT(QuantityOnHand) AS Count_Of_Records
+FROM INVENTORY;
+
+
+-- 2.35
+-- COUNT counts the number of rows (or non-NULL values) in a column.
+-- SUM adds up all the numeric values in a column.
+-- If QuantityOnHand values are: 5, 10, 20 → SUM returns 35.
+
+-- 2.37
+SELECT 
+    WarehouseID,
+    SUM(QuantityOnHand) AS TotalItemsOnHand
+FROM INVENTORY
+GROUP BY WarehouseID
+ORDER BY TotalItemsOnHand DESC;
+
+-- 2.38
+SELECT 
+    WarehouseID,
+    SUM(QuantityOnHand) AS TotalItemsOnHandLT3
+FROM INVENTORY
+WHERE QuantityOnHand < 3
+GROUP BY WarehouseID
+ORDER BY TotalItemsOnHandLT3 DESC;
+
+SELECT 
+    WarehouseID,
+    SUM(QuantityOnHand) AS TotalItemsOnHandLT3
+FROM INVENTORY
+WHERE QuantityOnHand < 3
+GROUP BY WarehouseID
+HAVING COUNT(*) < 2
+ORDER BY TotalItemsOnHandLT3 DESC;
+
+-- 3.9
+-- The WHERE clause removes rows where QuantityOnHand >= 3 before grouping.
+-- WHERE QuantityOnHand < 3 → filter rows before grouping.
+-- The WHERE clause is applied first.
+-- why 
+-- WHERE filters individual rows before grouping.
+-- GROUP BY then groups the remaining rows.
+-- HAVING applies conditions after the grouping has been created.
+
+-- 2.40
+SELECT 
+    I.SKU,
+    I.SKU_Description,
+    I.WarehouseID,
+    W.WarehouseCity,
+    W.WarehouseState
+FROM INVENTORY I
+JOIN WAREHOUSE W
+    ON I.WarehouseID = W.WarehouseID
+WHERE W.WarehouseCity IN ('Atlanta', 'Bangor', 'Chicago');
+
+-- 2.41
+SELECT 
+    I.SKU,
+    I.SKU_Description,
+    I.WarehouseID,
+    W.WarehouseCity,
+    W.WarehouseState
+FROM INVENTORY I
+JOIN WAREHOUSE W
+    ON I.WarehouseID = W.WarehouseID
+WHERE W.WarehouseCity IN ('Atlanta', 'Bangor', 'Chicago');
+
+-- 2.42
+SELECT 
+    I.SKU,
+    I.SKU_Description,
+    I.WarehouseID,
+    W.WarehouseCity,
+    W.WarehouseState
+FROM INVENTORY I
+JOIN WAREHOUSE W
+    ON I.WarehouseID = W.WarehouseID
+WHERE W.WarehouseCity <> 'Atlanta'
+  AND W.WarehouseCity <> 'Bangor'
+  AND W.WarehouseCity <> 'Chicago';
+
+-- 2.43
+SELECT 
+    I.SKU,
+    I.SKU_Description,
+    I.WarehouseID,
+    W.WarehouseCity,
+    W.WarehouseState
+FROM INVENTORY I
+JOIN WAREHOUSE W
+    ON I.WarehouseID = W.WarehouseID
+WHERE W.WarehouseCity NOT IN ('Atlanta', 'Bangor', 'Chicago');
+
+-- 2.44
+
+SELECT 
+    CONCAT(SKU_Description, ' is located in ', WarehouseCity) AS ItemLocation
+FROM INVENTORY I
+JOIN WAREHOUSE W
+    ON I.WarehouseID = W.WarehouseID;
+
+-- 2.45
+SELECT 
+    SKU, 
+    SKU_Description, 
+    WarehouseID
+FROM INVENTORY
+WHERE WarehouseID IN (
+    SELECT WarehouseID 
+    FROM WAREHOUSE
+    WHERE ManagerName = 'Lucille Smith'
+);
+
+-- 2.46
+SELECT 
+    I.SKU,
+    I.SKU_Description,
+    I.WarehouseID
+FROM INVENTORY I, WAREHOUSE W
+WHERE I.WarehouseID = W.WarehouseID
+  AND W.ManagerName = 'Lucille Smith';
+
+-- 2.47
+SELECT 
+    I.SKU,
+    I.SKU_Description,
+    I.WarehouseID
+FROM INVENTORY I
+JOIN WAREHOUSE W 
+    ON I.WarehouseID = W.WarehouseID
+WHERE W.ManagerName = 'Lucille Smith';
+
+-- 2.48
+SELECT 
+    WarehouseID,
+    AVG(QuantityOnHand) AS AvgQty
+FROM INVENTORY
+WHERE WarehouseID IN (
+    SELECT WarehouseID
+    FROM WAREHOUSE
+    WHERE ManagerName = 'Lucille Smith'
+)
+GROUP BY WarehouseID;
+
+-- 2.49
+SELECT 
+    I.WarehouseID,
+    AVG(I.QuantityOnHand) AS AvgQty
+FROM INVENTORY I, WAREHOUSE W
+WHERE I.WarehouseID = W.WarehouseID
+  AND W.ManagerName = 'Lucille Smith'
+GROUP BY I.WarehouseID;
+
+-- 2.50
+SELECT 
+    I.WarehouseID,
+    AVG(I.QuantityOnHand) AS AvgQty
+FROM INVENTORY I
+JOIN WAREHOUSE W
+    ON I.WarehouseID = W.WarehouseID
+WHERE W.ManagerName = 'Lucille Smith'
+GROUP BY I.WarehouseID;
+
+
+
+
+
+
+
+
+    
+  
+
 
 
 
